f7aba42c9b78c7a7527489c5d4b388e523a2d321
victorx10 <vs469963@gmail.com>
2025-10-25 02:40:37 -0300

    kkkkkkk

--- amazon-affiliate/src/main.jsx
+++ amazon-affiliate/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
       <App />
-    </HelmetProvider>
   </StrictMode>
 );
